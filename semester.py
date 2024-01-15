52254cb7aff500b3426282cee4e3dba9ac6a7b33
AnishaRana05 <151612671+AnishaRana05@users.noreply.github.com>
2024-01-15 18:10:25 +0500

    Delete semester.py

--- semester.py
+++
@@ -1,9 +0,0 @@
-from university import University
-
-class Semester:
-    def __init__(self, name):
-        self.name = name
-        self.allocated_courses = []
-
-    def allocate_course(self, course):
-        self.allocated_courses.append(course)
