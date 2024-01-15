726441f719240c4245af76e392dd708b32840af0
AnishaRana05 <151612671+AnishaRana05@users.noreply.github.com>
2024-01-15 18:09:01 +0500

    Delete university.py

--- university.py
+++
@@ -1,7 +0,0 @@
-class University:
-    def __init__(self, name):
-        self.name = name
-        self.semesters = []
-
-    def add_semester(self, semester):
-        self.semesters.append(semester)
